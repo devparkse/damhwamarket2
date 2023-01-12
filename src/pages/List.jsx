355c71e507d88caa447a0c67eb7fd32a95b060fa
Park Sieun <pmj8856@naver.com>
2023-01-12 18:19:28 +0900

    폰트 적용

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -5,9 +5,9 @@ const List = () => {
   const [list, setList] = useState([
     {
       img: "https://d38cxpfv0ljg7q.cloudfront.net/admin_contents/detail/v2uF-1672974858463-1.jpg",
-      name: "sul",
-      price: 1000,
-      subName: "sulggun",
+      name: "제주 동백",
+      price: "10,800원",
+      subName: "#화이트 와인 같은 산뜻함",
       star: 4.7,
     },
     {
@@ -62,7 +62,7 @@ const List = () => {
   ]);
 
   return (
-    <div className="grid grid-cols-4">
+    <div className="grid grid-cols-4 max-w-screen-xl m-auto">
       {list.map((item, index) => (
         <ListItem key={index} item={item} />
       ))}
