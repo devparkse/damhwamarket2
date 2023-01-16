384c9ebc5d64ff00086aeb9fe49f4cfef663efe4
hj.over <dhfldhs0707@gmail.com>
2023-01-16 16:46:31 +0900

    푸터 작성중

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="">
+      Footer
+      <footer className="footer">
+        <div className="grid wrapper">
+          <b className="text-lg"> 별별컴퍼니 주식회사 </b>
+          <br />
+          <div className="grid gap-y-3 text-sm">
+            고객센터: 010-1010-1010 <br />
+            평일 10:00 - 18:00, 주말 휴무
+            <div>
+              <b>
+                <Link to>이용약관</Link>&nbsp; <Link to> 개인정보처리방침</Link>{" "}
+                &nbsp;
+                <Link>입점문의</Link>
+              </b>
+            </div>
+            <p>
+              대표: 이성계 &nbsp;&nbsp;사업자등록번호 : 620-81-58299&nbsp;&nbsp;{" "}
+              <Link to>
+                <u>사업자확인</u>
+              </Link>{" "}
+              &nbsp;&nbsp;통신판매 신고번호: 2021-고려조선-1111
+            </p>
+            <p>
+              주소 : 대구광역시 중구 중앙대로 394, 제일빌딩 5F &nbsp;&nbsp;
+              대표전화 : 053.572.1005&nbsp;&nbsp; 이메일 : info@green.com
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
