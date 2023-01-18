30dc7ef0b8fb58bc3632381ba8c33df89a4ddbcd
tmdgus95 <8571053@naver.com>
2023-01-18 16:45:27 +0900

    스타일 수정

--- src/components/MyPageOption.jsx
+++ src/components/MyPageOption.jsx
@@ -8,11 +8,17 @@ const MyPageOption = ({ changePage }) => {
         changePage(false);
     };
     return (
-        <div className="flex flex-col justify-center self-center w-32 text-center absolute top-96 left-48 h-20 p-4 mr-4 bg-zinc-100  text-cyan-600">
-            <p onClick={handleChangeOrderDetail} className="border-b-2 mb-1">
+        <div className="flex flex-col justify-center self-center w-32 text-center absolute top-96 left-48 h-28 p-4 mr-4 bg-zinc-100  text-blue">
+            <p
+                onClick={handleChangeOrderDetail}
+                className="border-b-3 pb-1 mb-2  border-blue"
+            >
                 회원정보
             </p>
-            <p onClick={handleChangeUserInfo} className="border-b-2 mb-1">
+            <p
+                onClick={handleChangeUserInfo}
+                className="border-b-3 pb-1 mb-2  border-blue"
+            >
                 주문내역
             </p>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,23 @@ module.exports = {
         minHeight: {
             "1/2": "600px",
         },
+        borderWidth: {
+            DEFAULT: "1px",
+            0: "0",
+            2: "2px",
+            3: "3px",
+            4: "4px",
+            6: "6px",
+            8: "8px",
+        },
         extend: {
             width: {
                 imgwidth: "280px",
                 filterwidth: "350px",
             },
-            colors: {},
+            colors: {
+                blue: "#3395ed",
+            },
             fontWeight: {
                 fw800: 800,
             },
